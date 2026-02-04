f26b979b3c55382a247bb50a653c473e03b3a53a
chtaha1995-sys <chtaha1995@gmail.com>
2026-02-04 14:02:54 +0300

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,9 +1,9 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://kqogqronicwjjgzzcicd.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtxb2dxcm9uaWN3ampnenpjaWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNDQxNTksImV4cCI6MjA4NTcyMDE1OX0.LDofnQ_2bynQ7rjH2Gpt7w1z8Xefk_tTd5nY98LV04M";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtxb2dxcm9uaWN3ampnenpjaWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNDQxNTksImV4cCI6MjA4NTcyMDE1OX0.LDofnQ_2bynQ7rjH2Gpt7w1z8Xefk_tTd5nY98LV04M";
 
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
-);
